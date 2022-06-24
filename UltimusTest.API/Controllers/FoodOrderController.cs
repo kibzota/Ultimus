686f382cdc97b6ee0a9571de54a69facd3e29ed1
Danilo Soares <danilo3457@gmail.com>
2022-06-24 09:26:54 -0300

    created FoodOrderController

---
+++ UltimusTest.API/Controllers/FoodOrderController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UltimusTest.Domain.Interfaces.Services;
+
+namespace UltimusTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodOrderController: ControllerBase
+    {
+        private readonly IFoodOrderService _foodService;
+        public FoodOrderController(IFoodOrderService foodService) 
+        {
+            _foodService = foodService;
+        }
+
+        [HttpGet("order/{orderCSV}")]
+        public IActionResult GetAll(string orderCSV)
+        {
+            try
+            {
+                var response = _foodService.Execute(orderCSV);
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Bad Request: {ex.Message}");
+            }
+        }
+    }
+}
